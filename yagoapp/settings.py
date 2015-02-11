6d42d102a6dd33eb70f1257cfee45fe386a836e7
Bowden Kelly <bowdenk7@gmail.com>
2015-02-10 23:16:29 -0500

    Static files..

--- yagoapp/settings.py
+++ yagoapp/settings.py
@@ -92,6 +92,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
     'markdown',
