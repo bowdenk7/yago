e84a8f4d86c5bc20c3a50334a744e0491b013958
Bowden Kelly <bowdenk7@gmail.com>
2015-03-26 07:13:11 -0400

    More csrf work.

--- user_post/urls.py
+++ user_post/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 from user_post import views
 from yagoapp.urls import router
 
@@ -7,9 +8,9 @@ router.register(r'likes', views.LikeViewSet)
 
 urlpatterns = patterns('',
 
-    url(r'^like_post/$', views.like_post),
-    url(r'^report_post/$', views.report_post),
+    url(r'^like_post/$', csrf_exempt(views.like_post)),
+    url(r'^report_post/$', csrf_exempt(views.report_post)),
 
-    url(r'^$', views.PostList.as_view()),
-    url(r'^(?P<pk>[0-9]+)/$', views.PostDetail.as_view()),
+    url(r'^$', csrf_exempt(views.PostList.as_view())),
+    url(r'^(?P<pk>[0-9]+)/$', csrf_exempt(views.PostDetail.as_view())),
 )
