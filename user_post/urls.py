b58bc80e3087ab620947da93c46cc611f05e4353
Bowden Kelly <bowdenk7@gmail.com>
2015-03-26 01:27:14 -0400

    Removed old urls that aren't being used

--- user_post/urls.py
+++ user_post/urls.py
@@ -2,13 +2,11 @@ from django.conf.urls import patterns, url
 from user_post import views
 from yagoapp.urls import router
 
-router.register(r'posts', views.PostViewSet)
 router.register(r'reported_posts', views.ReportedPostViewSet)
 router.register(r'likes', views.LikeViewSet)
 
 urlpatterns = patterns('',
-
-    url(r'^create_post/$', views.create_post),
+                       
     url(r'^like_post/$', views.like_post),
     url(r'^report_post/$', views.report_post),
 
