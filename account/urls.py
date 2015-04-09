4669cb40f6eb7bf088096140736c2be38f3c996e
Bradley Oesch <bradley.oesch@gmail.com>
2015-04-09 17:48:08 -0400

    created feeds for all users, posts by user, likes by user, recent user posts, top user posts by likes, and promotions by user

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 from account import views
 from yagoapp.urls import router
 
@@ -7,7 +8,14 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = patterns('',
 
-    url(r'^register-by-token/$', views.social_register, name='register')
+    url(r'^register-by-token/$', views.social_register, name='register'),
+    url(r'^users_feed/$', csrf_exempt(views.get_users_feed)),
+    url(r'^users_feed/(?P<limit>[0-9]+)$', csrf_exempt(views.get_limited_users_feed)),
+    url(r'^recent_user_posts/(?P<user>[0-9]+)$', csrf_exempt(views.get_recent_user_posts)),
+    url(r'^top_user_posts/(?P<user>[0-9]+)$', csrf_exempt(views.get_top_user_posts)),
+    url(r'^user_likes/(?P<user>[0-9]+)$', csrf_exempt(views.get_user_likes)),
+    url(r'^user_promotion_feed/(?P<pk>[0-9]+)$', csrf_exempt(views.get_user_promotion_feed)),
+    url(r'^user_redeemed_promotion_feed/(?P<pk>[0-9]+)$', csrf_exempt(views.get_user_redeemed_promotion_feed)),
 
     # Attempt to register a new user
     # url(r'^register/$', views.register_user, name='register'),
--- account/views.py
+++ account/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.db.models import Count
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
-from social.apps.django_app.utils import strategy, psa
+from social.apps.django_app.utils import psa
 from account.serializers import UserSerializer
 from rest_framework.response import Response
+from promotion.models import Promotion
+from promotion.serializers import PromotionSerializer
+from user_post.models import Post, Like
+from user_post.serializers import PostSerializer, LikeSerializer
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -35,4 +40,87 @@ def auth_by_token(request, backend):
 def social_register(request):
     user = auth_by_token(request, "facebook")  # hard coded facebook because that's all we plan to support atm
     serializer = UserSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(['GET'])
+def get_users_feed(request):
+    """
+    Returns a list of all users ordered by private key
+    """
+    promotions = User.objects.all().order_by('-pk')
+    serializer = UserSerializer(promotions, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(['GET'])
+def get_limited_users_feed(request, limit):
+    """
+    Returns a list of users of size limit, ordered by private key
+    """
+    if limit > 0:
+        promotions = User.objects.all().order_by('-pk')
+        serializer = UserSerializer(promotions[:limit], many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return get_users_feed(request)
+
+
+@csrf_exempt
+@api_view(['GET'])
+def get_recent_user_posts(request, user):
+    """
+    Returns a list of all posts made by a user, newest first
+    """
+    posts = Post.objects.filter(user=user).annotate(Count("like")).order_by('-timestamp')
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(['GET'])
+def get_top_user_posts(request, user):
+    """
+    Returns a list of all posts made by a user, most likes first
+    """
+    posts = Post.objects.filter(user=user).annotate(Count("like")).order_by('-like__count')
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(['GET'])
+def get_user_likes(request, user):
+    """
+    Returns a list of all posts liked by a user
+    """
+    likes = Like.objects.filter(user=user).annotate(Count("post")).order_by('-post__count')
+    serializer = LikeSerializer(likes, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(['GET'])
+def get_user_promotion_feed(request, pk):
+    """
+    Returns a list of all unredeemed promotions "purchased" by a particular user
+
+    e.g. input user ID, get back promotions about free cover and discounts that have not yet been redeemed
+    """
+    promotions = Promotion.objects.filter(user=pk, redeemed=False).order_by('-expiration')
+    serializer = PromotionSerializer(promotions, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(['GET'])
+def get_user_redeemed_promotion_feed(request, pk):
+    """
+    Returns a list of all unredeemed promotions "purchased" by a particular user
+
+    e.g. input user ID, get back promotions about free cover and discounts that have not yet been redeemed
+    """
+    promotions = Promotion.objects.filter(user=pk, redeemed=True).order_by('-expiration')
+    serializer = PromotionSerializer(promotions, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
