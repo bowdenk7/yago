b82377b5d2eee1dd4a2d87d837f41c702a5dc3ae
Bowden Kelly <bowdenk7@gmail.com>
2015-04-13 23:07:14 -0400

    Multipart form support.

--- user_post/views.py
+++ user_post/views.py
@@ -6,6 +6,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
+from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from feed.models import Venue
@@ -26,6 +27,7 @@ class PostList(APIView):
     position
     venue
     """
+    parser_classes = (MultiPartParser,)
 
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
