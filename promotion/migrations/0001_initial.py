2f8b5134e91117516679bcd303621570f260c09f
Bowden Kelly <bowdenk7@gmail.com>
2015-04-16 06:18:35 -0400

    Added migrations and added user to admin panel.

---
+++ promotion/migrations/0001_initial.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('feed', '__first__'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Promotion',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('timestamp', models.DateTimeField(auto_created=True)),
+                ('expiration', models.DateTimeField(null=True, blank=True)),
+                ('redeemed', models.BooleanField(default=False)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='PromotionType',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=120)),
+                ('description', models.CharField(max_length=400)),
+                ('point_cost', models.IntegerField()),
+                ('venue', models.ForeignKey(to='feed.Venue')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='promotion',
+            name='type',
+            field=models.ForeignKey(to='promotion.PromotionType'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='promotion',
+            name='user',
+            field=models.ForeignKey(related_name='promotions', to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
+        ),
+    ]
